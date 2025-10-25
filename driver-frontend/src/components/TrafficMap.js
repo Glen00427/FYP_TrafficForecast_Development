7a52326d105acbff0dcea70e8243eee2cfc90122
ying117 <shjy117@gmail.com>
2025-10-25 21:43:50 +0800

    Update TrafficMap to fetch and display incidents
    Refactored TrafficMap component to fetch incidents and display them on the map.

--- driver-frontend/src/components/TrafficMap.js
+++ driver-frontend/src/components/TrafficMap.js
@@ -1,7 +1,19 @@
 import React from "react";
 import LiveTrafficMap from "./LiveTrafficMap";
+import { fetchIncidents } from "./fetchIncidents";
 
-export default function TrafficMap({ selectedRoute, incidents = [] }) {
+
+export default function TrafficMap({ selectedRoute }) {
+
+  const [incidents, setIncidents] = useState([]);
+
+  useEffect(() => {
+    const loadIncidents = async () => {
+      const data = await fetchIncidents();
+      setIncidents(data);
+    };
+    loadIncidents();
+    
   return (
     <div style={{ position: "absolute", inset: 0 }}>
       <LiveTrafficMap
@@ -34,3 +46,4 @@ export default function TrafficMap() {
   );
 }
 */
+
