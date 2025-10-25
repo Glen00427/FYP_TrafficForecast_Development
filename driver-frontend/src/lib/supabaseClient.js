f51d02e1e7d5b68f050f7bef138fc5739a6365cb
ying117 <shjy117@gmail.com>
2025-10-25 21:37:30 +0800

    Update supabaseClient.js

--- driver-frontend/src/lib/supabaseClient.js
+++ driver-frontend/src/lib/supabaseClient.js
@@ -6,9 +6,3 @@ const SUPABASE_ANON_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indlc3Njd3pyc3V5aXFkZWNjeXNtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc1NzEwOTMsImV4cCI6MjA3MzE0NzA5M30.5BRLFWRcKaQlNUpOPRwVjP51M3kXaDH4YadqwY5Qmx0";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
-// second supabase testing for incidents from LTA
-const SUPABASE_URL_SECOND = "https://vxistpqjjavwykdsgeur.supabase.co";
-const SUPABASE_KEY_SECOND = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ4aXN0cHFqamF2d3lrZHNnZXVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkwODMzNTQsImV4cCI6MjA3NDY1OTM1NH0.2JfYXhZuL6wKEJMZV_LRcFgAr3xguLkgegxr5V7_u1Y";
-
-export const supabase_second = createClient(SUPABASE_URL_SECOND, SUPABASE_KEY_SECOND);
